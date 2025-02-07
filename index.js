f9ca179435be3152ac9a3ea89f1ba5355d059649
unknown <YY411@ccs.local>
2025-02-07 05:25:17 -0800

    Final Output

--- index.js
+++ index.js
@@ -2,19 +2,51 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" folder
+//Serve static files from the "public" folder
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/about', (req,res) => {
-  res.send('About Us');
-});
+
+app.post('/submit',(req,res )=> {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`)
+})
 
 //Define a route for the home page
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req,res)=>  {
+    res.send('Hello, World!')
 })
 
-//Start the server
-app.listen(port, () => {
-  console.log(`Service running at http:localhost:${port}`);
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+const items = ['Apple','Banana','Orange'];
+
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
 })
+
+app.get('/about', (req,res) => {
+    res.send('About Us')
+})
+
+
+//Start the server
+app.listen(port, ()=> {
+    console.log(`Sever running at http://localhost:${port}`);
+})
+
+//Logging Middleware
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+//Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
